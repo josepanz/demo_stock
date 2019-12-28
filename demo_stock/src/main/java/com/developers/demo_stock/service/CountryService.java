066be2890ee362e115dbc016a451f6c537b75e2d
César Agustín Ozuna <agustinozuna@hotmail.com>
2019-12-28 11:22:12 -0300

    Spring boot cambio

---
+++ demo_stock/src/main/java/com/developers/demo_stock/service/CountryService.java
@@ -0,0 +1,25 @@
+package com.developers.demo_stock.service;
+
+
+
+import java.util.Optional;
+
+import com.developers.demo_stock.entity.Country;
+
+public interface CountryService  {
+	
+public Iterable<Country> getAllCountry();
+
+public void save(Country country);
+
+public void delete(Integer id);
+
+Optional<Country> findById(Integer id);
+
+public Country getCountryById(Integer id) ;  
+
+public Country updateCountry(Country country);
+
+
+
+}
